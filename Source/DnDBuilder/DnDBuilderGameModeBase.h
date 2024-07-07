75a31140a0cfd8ca6790df12fd1ea218d6d25a36
Thomas Bernier <thomas44bernier@gmail.com>
2024-07-07 14:59:50 -0400

    first project commit

---
+++ Source/DnDBuilder/DnDBuilderGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "DnDBuilderGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class DNDBUILDER_API ADnDBuilderGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
