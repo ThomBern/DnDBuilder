75bbef19f63960d8b223f925f43a43e455954b28
Thomas Bernier <thomas44bernier@gmail.com>
2024-07-09 22:23:16 -0400

    Vidéo 3 - Contrôle de la caméra

--- Source/DnDBuilder/Core/Player/DDB_Pawn.cpp
+++ Source/DnDBuilder/Core/Player/DDB_Pawn.cpp
@@ -3,11 +3,31 @@
 
 #include "DDB_Pawn.h"
 
+#include "DDB_PlayerController.h"
+#include "Camera/CameraComponent.h"
+#include "GameFramework/SpringArmComponent.h"
+#include "EnhancedInputComponent.h"
+#include "EnhancedInputSubsystems.h"
+#include "Math/UnrealMathUtility.h"
+
 // Sets default values
 ADDB_Pawn::ADDB_Pawn()
-{
+{	
+
  	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
+
+	SceneRoot = CreateDefaultSubobject<USceneComponent>(TEXT("SceneRoot"));
+	SetRootComponent(SceneRoot);
+
+	SpringArm = CreateDefaultSubobject<USpringArmComponent>(TEXT("SpringArm"));
+	SpringArm->SetupAttachment(RootComponent);
+	SpringArm->TargetArmLength = 800.f;
+	SpringArm->SetRelativeRotation(FRotator(-60.f, 0.f, 0.f));
+	SpringArm->bDoCollisionTest = false; // Don't want to pull camera in when it collides with level
+
+    Camera = CreateDefaultSubobject<UCameraComponent>(TEXT("Camera"));
+	Camera->SetupAttachment(SpringArm, USpringArmComponent::SocketName);
 
 }
 
@@ -15,6 +35,10 @@ ADDB_Pawn::ADDB_Pawn()
 void ADDB_Pawn::BeginPlay()
 {
 	Super::BeginPlay();
+
+	locationDesired = GetActorLocation();
+	zoomDesired = SpringArm->TargetArmLength;
+	rotationDesired = GetActorRotation();
 	
 }
 
@@ -23,6 +47,12 @@ void ADDB_Pawn::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
 
+	
+	SpringArm->TargetArmLength = FMath::FInterpTo(SpringArm->TargetArmLength, zoomDesired, DeltaTime, 2);
+
+	SetActorLocation(FMath::VInterpTo(GetActorLocation(), locationDesired, DeltaTime, 5));
+
+	SetActorRotation(FMath::RInterpTo(GetActorRotation(), rotationDesired, DeltaTime, 5));
 }
 
 // Called to bind functionality to input
@@ -30,5 +60,47 @@ void ADDB_Pawn::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
 {
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
 
+	if (TObjectPtr<UEnhancedInputComponent> EnhancedInputComponent = CastChecked<UEnhancedInputComponent>(PlayerInputComponent))
+	{
+		// Get the local player subsystem
+		UEnhancedInputLocalPlayerSubsystem* InputSubsystem =
+			ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(Cast<ADDB_PlayerController>(GetOwner())->GetLocalPlayer());
+
+		InputSubsystem->AddMappingContext(InputMappingContext, 1);
+
+		EnhancedInputComponent->BindAction(ZoomAction, ETriggerEvent::Triggered, this, &ADDB_Pawn::HandleZoom);
+		EnhancedInputComponent->BindAction(ForwardAction, ETriggerEvent::Triggered, this, &ADDB_Pawn::HandleForward);
+		EnhancedInputComponent->BindAction(RightAction, ETriggerEvent::Triggered, this, &ADDB_Pawn::HandleRight);
+		EnhancedInputComponent->BindAction(RotationRightAction, ETriggerEvent::Triggered, this, &ADDB_Pawn::HandleRotationRight);
+		EnhancedInputComponent->BindAction(RotationLeftAction, ETriggerEvent::Triggered, this, &ADDB_Pawn::HandleRotationLeft);
+		
+	}
 }
 
+
+void ADDB_Pawn::HandleZoom(const FInputActionValue& value)
+{
+	// GEngine->AddOnScreenDebugMessage(-1, 5.f, FColor::Red, FString::Printf(TEXT("%f"), value.Get<float>()));
+
+	zoomDesired = FMath::Clamp(zoomDesired + value.Get<float>() * 40, 100.f, 2000.f);
+}
+
+void ADDB_Pawn::HandleForward(const FInputActionValue& value)
+{
+	locationDesired = locationDesired + GetActorForwardVector() * value.Get<float>() * 20;
+}
+
+void ADDB_Pawn::HandleRight(const FInputActionValue& value)
+{
+	locationDesired = locationDesired + GetActorRightVector() * value.Get<float>() * 20;
+}
+
+void ADDB_Pawn::HandleRotationRight(const FInputActionValue& value)
+{
+	rotationDesired.Yaw = rotationDesired.Yaw - 45.f; 
+}
+
+void ADDB_Pawn::HandleRotationLeft(const FInputActionValue& value)
+{
+	rotationDesired.Yaw = rotationDesired.Yaw + 45.f; 
+}
--- Source/DnDBuilder/Core/Player/DDB_Pawn.h
+++ Source/DnDBuilder/Core/Player/DDB_Pawn.h
@@ -4,7 +4,13 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/Pawn.h"
+#include "InputAction.h"
+#include "InputActionValue.h"
+
 #include "DDB_Pawn.generated.h"
+
+class UCameraComponent;
+class USpringArmComponent;
 
 UCLASS()
 class DNDBUILDER_API ADDB_Pawn : public APawn
@@ -15,15 +21,59 @@ public:
 	// Sets default values for this pawn's properties
 	ADDB_Pawn();
 
-protected:
-	// Called when the game starts or when spawned
-	virtual void BeginPlay() override;
-
-public:	
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
 
 	// Called to bind functionality to input
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Player Input")
+	TObjectPtr<UInputMappingContext> InputMappingContext = nullptr;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Player Input|Camera Movement")
+	TObjectPtr<UInputAction> ZoomAction = nullptr;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Player Input|Camera Movement")
+	TObjectPtr<UInputAction> ForwardAction = nullptr;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Player Input|Camera Movement")
+	TObjectPtr<UInputAction> RightAction = nullptr;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Player Input|Camera Movement")
+	TObjectPtr<UInputAction> RotationRightAction = nullptr;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Player Input|Camera Movement")
+	TObjectPtr<UInputAction> RotationLeftAction = nullptr;
+
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+	void HandleZoom(const FInputActionValue& value);
+	void HandleForward(const FInputActionValue& value);
+	void HandleRight(const FInputActionValue& value);
+	void HandleRotationRight(const FInputActionValue& value);
+	void HandleRotationLeft(const FInputActionValue& value);
+
+private:
+
+	UPROPERTY(VisibleAnywhere)
+	float zoomDesired;
+
+	UPROPERTY(VisibleAnywhere)
+	FVector locationDesired;
+
+	UPROPERTY(VisibleAnywhere)
+	FRotator rotationDesired;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera, meta = (AllowPrivateAccess = "true"))
+	TObjectPtr<USceneComponent> SceneRoot;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera, meta = (AllowPrivateAccess = "true"))
+	TObjectPtr<USpringArmComponent> SpringArm;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera, meta = (AllowPrivateAccess = "true"))
+	TObjectPtr<UCameraComponent> Camera;
 
 };
